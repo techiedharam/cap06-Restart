9b5e751444368572ef6f470dc88f14129f8635d4
Dharmender <dharmender7077@gmail.com>
2024-04-28 17:56:27 +0530

    Cap Retstart Evaluation 28-04-2024

--- Restart-Evaluations/Final-Evaluation-28-4-2024/script.js
+++ Restart-Evaluations/Final-Evaluation-28-4-2024/script.js
@@ -1,151 +1,88 @@
-// Catch the Element 
-const EmployeeContainer = document.getElementById('employee-container')
-const EmpTable = document.getElementById('EmpTable')
-const EmpBody = document.getElementById('tbody')
+// script.js
 
-let showEmployees = (EmpList) => {
+const EmpBody = document.getElementById('tbody');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+let currentPage = 1;
+const limit = 10; // Number of items per page
+
+const showEmployees = (EmpList) => {
     EmpBody.innerHTML = "";
 
-    EmpList.forEach((emp)=> {
-
+    EmpList.forEach((emp, index) => {
         const tRow = document.createElement('tr');
 
-        const SNotd = document.createElement('td')
-            SNotd.textContent = emp.id
-        const Nametd = document.createElement('td')
-            Nametd.textContent = emp.name;
-        const Gendertd = document.createElement('td')
-            Gendertd.textContent = emp.gender
-        const Departmenttd = document.createElement('td')
-            Departmenttd.textContent = emp.department;
-        const Salarytd = document.createElement('td')
-            Salarytd.textContent = emp.salary;
-        
-            tRow.append(
-                SNotd ,
-                Nametd,
-                Gendertd,
-                Departmenttd,
-                Salarytd
-            )
-            EmpBody.appendChild(
-                tRow
-            )
-            
-            EmpTable.appendChild(
-                EmpBody
-            )
-    })
+        const SNotd = document.createElement('td');
+        SNotd.textContent = (currentPage - 1) * limit + index + 1;
 
-}
+        const Nametd = document.createElement('td');
+        Nametd.textContent = emp.name;
 
+        const Gendertd = document.createElement('td');
+        Gendertd.textContent = emp.gender;
 
-const GetEmpData = async (sortValue , deptFilterValue , genderFiterValue) => {
-    let URL = "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=1&limit=10";
+        const Departmenttd = document.createElement('td');
+        Departmenttd.textContent = emp.department;
+
+        const Salarytd = document.createElement('td');
+        Salarytd.textContent = emp.salary;
+
+        tRow.append(SNotd, Nametd, Gendertd, Departmenttd, Salarytd);
+        EmpBody.appendChild(tRow);
+    });
+};
+
+const GetEmpData = async (sortValue, deptFilterValue, genderFiterValue, page) => {
     
-    if(sortValue) {
+    let URL = `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=1&limit=10`;
+    if(page) {
+        let URL = `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${page}&limit=${limit}`;
+    }
+    
+    if (sortValue) {
         URL += `&sort=salary&order=${sortValue}`;
     }
-    if(deptFilterValue) {
+    if (deptFilterValue) {
         URL += `&filterBy=department&filterValue=${deptFilterValue}`;
     }
-    if(genderFiterValue) {
+    if (genderFiterValue) {
         URL += `&filterBy=gender&filterValue=${genderFiterValue}`;
     }
 
     try {
-        const response = await fetch(URL)
-        const finalResponse = await response.json()
-        showEmployees(finalResponse.data)
+        const response = await fetch(URL);
+        const finalResponse = await response.json();
+        showEmployees(finalResponse.data);
     } catch (error) {
-        console.log(error)
-    }
-}
+        console.log(error);
+    }
+};
 
-GetEmpData()
+GetEmpData();
 
-// Sorting Logic Here 
-const sortOrder = document.getElementById('salary-sort')
-sortOrder.addEventListener('change' , () => {
-    GetEmpData(sortOrder.value , '' , '')
-})
+const sortOrder = document.getElementById('salary-sort');
+sortOrder.addEventListener('change', () => {
+    GetEmpData(sortOrder.value, '', '', currentPage);
+});
 
+const deptFilter = document.getElementById('dept-filter');
+deptFilter.addEventListener('change', () => {
+    GetEmpData('', deptFilter.value, '', currentPage);
+});
 
-// Department Filter Logic here 
-// Catch the element 
-const deptFilter = document.getElementById('dept-filter')
+const genderFilter = document.getElementById('gender-filter');
+genderFilter.addEventListener('change', () => {
+    GetEmpData('', '', genderFilter.value, currentPage);
+});
 
-deptFilter.addEventListener('change' , () => {
-    GetEmpData('' , deptFilter.value , '')
-})
+prevButton.addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        GetEmpData('', '', '', currentPage);
+    }
+});
 
-// Gender Filter Logic here 
-// Catch the element 
-const genderFilter = document.getElementById('gender-filter')
-
-genderFilter.addEventListener('change' , () => {
-    GetEmpData('' , '' , genderFilter.value)
-    // console.log(genderFilter.value)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const GetEmpData = async (sortValue , deptFilterValue , genderFiterValue , ) => {
-//     let URL = "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees";
-    
-//     if(sortValue) {
-//         URL = `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=1&limit=100&sort=salary&order=${sortValue}`;
-//     }
-//     if(deptFilterValue) {
-//         URL = `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=1&limit=100&sortBy=salary&sortOrder=asc&filterBy=department&filterValue=${deptFilterValue}`
-//     }
-//     if(genderFiterValue) {
-//         URL = `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=1&limit=100&sortBy=salary&sortOrder=asc&filterBy=gender&filterValue=${genderFiterValue}`
-//     }
-    
-    
-//     // if(sortValue || deptFilterValue || genderFiterValue) {
-//     //     URL = `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=1&limit=10&sortBy=salary&sortOrder=${sortValue}&filterBy=department&filterValue=${deptFilterValue}&filterBy=gender&filterValue=${genderFiterValue}`;
-
-//     // }
-
-//     try {
-//         const response = await fetch(URL)
-//         const finalResponse = await response.json()
-//         showEmployees(finalResponse.data)
-//         // console.log(finalResponse)
-//     } catch (error) {
-//         console.log(error)
-//     }
-    
-// }
+nextButton.addEventListener('click', () => {
+    currentPage++;
+    GetEmpData('', '', '', currentPage);
+});
